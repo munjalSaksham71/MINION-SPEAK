0433d3312425d400ed099ceb61aedb7b0429e308
munjalSaksham71 <sakshammunjal4@gmail.com>
2021-01-05 22:03:13 +0530

    Update App.js

--- App.js
+++ App.js
@@ -1,10 +1,26 @@
 var btnTranslate = document.querySelector("#btn-translate");
 var txtInput = document.querySelector("#txt-input");
-var txtOutput = document.querySelector("#output")
+var txtOutput = document.querySelector("#output");
 
+var serverURL = "https://api.funtranslations.com/translate/minion.json"
+
+
+function getTranslationURL(text) {
+    return serverURL + "? " + "text= " + text;
+}
  
 function clickHandler() {
-    txtOutput.innerHTML = "sahkjsh " + txtInput.value;
+    var inputText = txtInput.value; //taking Input
+
+    //Calling Server  For Processing 
+
+    fetch(getTranslationURL(inputText))
+    .then (response => response.json())
+    .then (json => {
+        var TranslatedText = json.contents.translated;
+        txtOutput.innerHTML = TranslatedText;
+    })
+
 }
 
 btnTranslate.addEventListener("click", clickHandler);
